8cef0b096e7416afd66291fd92eb99ddc702b44c
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-07-15 18:06:13 +0300

    Update main.js

--- Scientific Calculator/main.js
+++ Scientific Calculator/main.js
@@ -4,7 +4,7 @@ var equal = document.querySelector"("#eval");
 for(item of btn ){
  item.addEventListener('click', (e)=>{
   let btnValue = e.target.innerText;
-hh
+h
   if(btnValue == '÷'){
     btnValue = '/';
   }
