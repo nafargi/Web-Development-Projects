a16dcd206da1d62a154989a73b059089319aa0e2
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-07-16 01:43:12 +0300

    Add files via upload

---
+++ Neumorphism On Off button/main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', (event) =>{
+
+ const btnToggle = document.getElementById("btnToggle");
+ const span = btnToggle.querySelector('span');
+ const circle = btnToggle.querySelector('.circle');
+
+ btnToggle.addEventListener('click',() =>{
+    if(span.textContent === 'Off'){
+       span.textContent = 'On';
+       btnToggle.classList.add('on');
+    }else{
+      span.textContent ='Off';
+      btnToggle.classList.remove('on');
+    }
+ });
+});
