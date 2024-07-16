1c47b597f2b44529b6bb485f3bde7351e3f2f5f0
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-07-17 00:31:12 +0300

    Update main.js

--- Neumorphism On Off button/main.js
+++ Neumorphism On Off button/main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) =>{
-hhhhh
+hhhh
  const btnToggle = document.getElementById("btnToggle");
  const span = btnToggle.querySelector('span');
  const circle = btnToggle.querySelector('.circle');
