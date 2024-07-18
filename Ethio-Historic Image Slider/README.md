cef9b22ca9b76d62b80006e1664d5d9a1c973f30
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-07-18 18:22:28 +0300

    Add files via upload

---
+++ Ethio-Historic Image Slider/README.md
@@ -0,0 +1,4 @@
+# Animated Imaged Slider HTML CSS & JavaScript 
+## Animated Carousel Design
+
+<img src="./img/img0.png">
---
+++ Ethio-Historic Image Slider/app.js
@@ -0,0 +1,41 @@
+
+let nextBtn = document.querySelector('.next')
+let prevBtn = document.querySelector('.prev')
+
+let slider = document.querySelector('.container')
+let sliderList = slider.querySelector('.container .class-list')
+let thumbnail = document.querySelector('.container .side-img')
+let thumbnailItems = thumbnail.querySelectorAll('.list')
+
+thumbnail.appendChild(thumbnailItems[0])
+
+nextBtn.onclick = function(){
+    moveSlider('next')
+}
+
+prevBtn.onclick = function(){
+    moveSlider('prev')
+}
+
+function moveSlider(direction){
+   let sliderItems = sliderList.querySelectorAll('.list')
+   let thumbnailItems = document.querySelectorAll('.side-img .list')
+
+   if(direction === 'next'){
+      sliderList.appendChild(sliderItems[0])
+      thumbnail.appendChild(thumbnailItems[0])
+      slider.classList.add('next')
+   }else{
+      sliderList.prepend(sliderItems[sliderItems.length -1 ])
+      thumbnail.prepend(thumbnailItems[thumbnailItems.length - 1])
+      slider.classList.add('prev')
+   }
+
+   slider.addEventListener('animationend', function(){
+    if(direction === 'next'){
+        slider.classList.remove('next')
+    }else{
+        slider.classList.remove('prev')
+    }
+   },{once: true})
+} 
