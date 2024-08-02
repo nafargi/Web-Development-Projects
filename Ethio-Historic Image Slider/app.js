a169a84294b6b303d58757bf27ad6e469a93202c
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-08-02 13:03:44 +0300

    Update app.js

--- Ethio-Historic Image Slider/app.js
+++ Ethio-Historic Image Slider/app.js
@@ -1,7 +1,7 @@
 
 let nextBtn = document.querySelector('.next')
 let prevBtn = document.querySelector('.prev')
-ooo
+oo
 let slider = document.querySelector('.container')
 let sliderList = slider.querySelector('.container .class-list')
 let thumbnail = document.querySelector('.container .side-img')
