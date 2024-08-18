6b0232a4d60b599d3327f1baa6246f0744699a6e
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-08-18 04:00:48 +0300

    Delete JavaScript 10 days mini project/Hello Name Application/script.js

--- JavaScript 10 days mini project/Hello Name Application/script.js
+++
@@ -1,7 +0,0 @@
-const btnElement = document.querySelector('button')
-const spanElement = document.getElementById('update')
-
-btnElement.onclick = function(){
-   const yourName = prompt("Please Enter Your Name :");
-   spanElement.textContent = yourName;
-}
