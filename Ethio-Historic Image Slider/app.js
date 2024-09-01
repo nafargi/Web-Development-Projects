205be54a4372f10b0b81f0dfd1c0a68933f5bf87
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-09-01 21:17:42 +0300

    Update app.js

--- Ethio-Historic Image Slider/app.js
+++ Ethio-Historic Image Slider/app.js
@@ -5,6 +5,7 @@ let sliderList = slider.querySelector('.container .class-list')
 let thumbnail = document.querySelector('.container .side-img')
 let thumbnailItems = thumbnail.querySelectorAll('.list')
 
+
 thumbnail.appendChild(thumbnailItems[0])
 nextBtn.onclick = function(){
     moveSlider('next')
